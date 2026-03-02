c68b3ff1a1a55bed5f87f4640a01e366a3048656
salsnablh <salsnabllh@gmail.com>
2026-03-02 14:48:59 +0700

    logoo

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -1,10 +1,10 @@
 <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon">
-                    <img src="<?= base_url('assets/img/logo.png'); ?>" width="50" alt="Logo">
+                    <img src="<?= base_url('assets/img/logo.png'); ?>" width="80" alt="Logo">
                 </div>
                 <div class="sidebar-brand-text mx-3">Santika</div>
             </a>
